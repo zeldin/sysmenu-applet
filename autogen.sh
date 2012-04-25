0dab0f23e5f8243ab9fa63588cfcc3bdcd10fb0c
Marcus Comstedt <marcus@mc.pp.se>
2012-04-25 23:07:59 +0200

    Added NEWS and ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 set -e
+git log --stat --no-color > ChangeLog
 intltoolize --automake --copy
 libtoolize -q
 aclocal -I m4
 autoheader
-automake --foreign --add-missing
+automake --add-missing
 autoconf
