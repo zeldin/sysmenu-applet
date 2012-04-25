5b37ab6c48b886a8923718c85056d8437be634a4
Marcus Comstedt <marcus@mc.pp.se>
2012-04-25 23:15:46 +0200

    autogen fixes
    * Copy files instead of linking them
    
    * Detect copies/moves in ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 set -e
-git log --stat --no-color > ChangeLog
+git log --stat --no-color -C -C -M > ChangeLog
 intltoolize --automake --copy
-libtoolize -q
+libtoolize -q --copy
 aclocal -I m4
 autoheader
-automake --add-missing
+automake --add-missing --copy
 autoconf
